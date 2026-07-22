abd84688ab4c078cbc07d38444685881f8db3dae
Kamatchi <Ramalingam.Kamatchi@wittur.com>
2026-07-22 15:52:38 +0530

    Initial commit

--- grade_system.py
+++ grade_system.py
@@ -1,11 +1,15 @@
+# Get the user Mark as input
 marks = input("Enter Student Marks (or A for Absent): ")
 
+# Validate if Ansent as A. user can input as A/a this will convert into uppercase
 if marks.upper() == "A":
     grade = "Absent"
+
+    # simply if we validate as isdigit it wont validate flot digit/text, it consider only whole number, so using replace to replace DOT to remove
     # marks.isdigit():
 elif marks.replace(".", "", 1).isdigit():
-    marks = float(marks)
 
+    marks = float(marks)
     if 90 <= marks <= 100:
         grade = "A"
     elif 80 <= marks < 90:
@@ -17,8 +21,7 @@ elif marks.replace(".", "", 1).isdigit():
     elif 0 <= marks < 60:
         grade = "E"
     else:
-        grade = "Invalid Marks"
+        grade = "Invalid Marks......s"
 else:
     grade = "Invalid Input"
-
 print("Result:", grade)
